5ce45c31e32f794731a83a52ea84798e0a7bc361
spexea <126134442+spexea@users.noreply.github.com>
2024-12-28 16:51:02 -0500

    Update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,12 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      // Desactiva todas las reglas
-      all: "off", // Esto desactiva todas las reglas
-    },
-  },
 ];
 
 export default eslintConfig;
